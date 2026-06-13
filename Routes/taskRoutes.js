7464f86e5b73746cff71e2ab8dc001704a36231d
jibexdbest999 <jibexdbest356@gmail.com>
2026-06-13 07:26:06 +0100

    first commit

---
+++ Routes/taskRoutes.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const Task = require("../Models/Task.js");
+
+router.get("/", async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await Task.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      message: "Task deleted",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/tasks", require("./routes/taskRoutes"));
+
+console.log("MONGO_URL =", process.env.MONGO_URL);
+mongoose
+.connect(process.env.MONGO_URL, {
+   serverSelectionTimeoutMS: 10000,
+})
+  .then(() => {
+    console.log("MongoDB Connected!!!");
+
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server running on port ${process.env.PORT || 5000}`);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+  
